001ed6c50c82466db3e396e9288e2d3240dd5830
Andrzej Senderecki <skidrzej@gmail.com>
2019-03-24 21:04:11 +0100

    Updates

---
+++ precache-manifest.86bfcd306dee347402242d1d7c90df2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "88ed86bff5ce9fa4b0ab",
+    "url": "/React_Sentence_Component/static/js/runtime~main.81295bd6.js"
+  },
+  {
+    "revision": "a1d6879372a811190d27",
+    "url": "/React_Sentence_Component/static/js/main.20c2f5ee.chunk.js"
+  },
+  {
+    "revision": "43aff7bda9488fc6bfd7",
+    "url": "/React_Sentence_Component/static/js/2.292baf1c.chunk.js"
+  },
+  {
+    "revision": "a1d6879372a811190d27",
+    "url": "/React_Sentence_Component/static/css/main.cb0642b3.chunk.css"
+  },
+  {
+    "revision": "44812ec6c6932446574339a5d4bf2c93",
+    "url": "/React_Sentence_Component/index.html"
+  }
+];
